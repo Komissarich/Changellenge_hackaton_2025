a2661c8ce1298973d5dbcbf340e4d0928fd07492
artemalo <artemphirsov@mail.ru>
2025-11-27 23:15:44 +0300

    create back

---
+++ xaxaton/src/main/java/com/example/xaxaton/XaxatonApplication.java
@@ -0,0 +1,13 @@
+package com.example.xaxaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XaxatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XaxatonApplication.class, args);
+	}
+
+}
